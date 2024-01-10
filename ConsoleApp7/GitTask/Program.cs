6c863ab72417e4e77804b6be0bc7dda36ff1f736
IvanvIvanv <148952411+IvanvIvanv@users.noreply.github.com>
2024-01-10 19:03:46 +0300

    Added a counter
    Added a counter

--- ConsoleApp7/GitTask/Program.cs
+++ ConsoleApp7/GitTask/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.WriteLine("Input a number for count");
+    try
+    {
+        int num = int.Parse(Console.ReadLine());
+        while (num > 0)
+        {
+            num--;
+            Console.WriteLine(num);
+        }
+        Console.WriteLine();
+    }
+    catch
+    {
+        Console.WriteLine("Caught exception");
+        Console.WriteLine();
+        continue;
+    }
+}
